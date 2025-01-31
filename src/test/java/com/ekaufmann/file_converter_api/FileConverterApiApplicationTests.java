a14cfe4f952995bf34e1ca217aa6a5f9bef8bd36
ekaufmann <51715734+ekaufmann@users.noreply.github.com>
2025-01-30 21:37:32 -0300

    initial setup

---
+++ src/test/java/com/ekaufmann/file_converter_api/FileConverterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekaufmann.file_converter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileConverterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
